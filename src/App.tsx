fdaf5b9ec8a15545f93e3c8a457a93596201b4d9
yt3trees <y.atforest@gmail.com>
2021-06-27 21:29:02 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import TaskList  from './components/TaskList'
+import TaskInput from './components/TaskInput'
+import { Task } from './components/Types'
 import './App.css';
 
-function App() {
+const initialState: Task[] = [
+  {
+    id: 1,
+    timefrom: "09:00",
+    timeto: "10:00",
+    todo: "テスト"
+  }
+]
+
+const App: React.FC = () => {
+  const [tasks, setTask] = useState(initialState)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TaskInput setTask={setTask} tasks={tasks} />
+      <TaskList setTask={setTask} tasks={tasks} />
     </div>
+
   );
 }
 
